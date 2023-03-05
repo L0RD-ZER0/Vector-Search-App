ff82ec76cc8c1cfae6fe668ce1df3757d5a5ed02
L0RD-ZER0 <68327382+L0RD-ZER0@users.noreply.github.com>
2023-03-06 02:36:58 +0530

    Configure urls for the project.

--- api/frontend/urls.py
+++ api/frontend/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='homepage'),
+    path('add', views.add, name='add-report'),
+    path('result', views.result, name='result'),
+    path('report', views.report, name='report'),
+    path('report/<int:report_id>/', views.view_report, name='homepage'),
 ]
--- api/frontend/views.py
+++ api/frontend/views.py
@@ -6,3 +6,27 @@ def home(request):
     Home-Page at `/`
     """
     return HttpResponse("It Works!")
+
+
+def add(request):
+    """
+    Add-Page at `/add`
+    """
+
+
+def result(request):
+    """
+    Result-Page at `/result`
+    """
+
+
+def report(request):
+    """
+    Report-Page at `/report`
+    """
+
+
+def view_report(request, report_id: int):
+    """
+    Individual-Report-Pages at `/report/<int:id>`
+    """
